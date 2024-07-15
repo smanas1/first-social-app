cc8aa5344fef5b2f323ce7eee0e614ef9a4cdead
Anas <anasolin01777@gmail.com>
2024-07-15 10:11:40 +0600

    show image

--- client/src/components/Home/Post/CreatePost.tsx
+++ client/src/components/Home/Post/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CircleCloseIcon } from "../../../assets/images/CircleClose";
 import Feeling from "../../../assets/images/Feeling";
 import { userData } from "../data";
@@ -9,12 +9,15 @@ import { CircleProfileIcon } from "../../../assets/images/Circleprofile";
 const CreatePost = () => {
   const [emoji, setEmoji] = useState(false);
   const [text, setText] = useState("");
+  const [imageShow, setImageShow] = useState(true);
+
+  const choseRef = useRef<HTMLInputElement>(null);
   const onEmojiClick = (emojiObject: EmojiClickData) => {
     setText((prevInput) => prevInput + emojiObject.emoji);
   };
   return (
     <div className="absolute h-screen z-40 w-full flex justify-center items-center  bg-black bg-opacity-30 ">
-      <div className="py-3 px-4 w-96 bg-white rounded-md">
+      <div className="py-3 px-4 w-[500px] bg-white rounded-md">
         <div className="relative">
           <h2 className="text-black text-center text-lg border-b py-2">
             Create Post
@@ -31,46 +34,98 @@ const CreatePost = () => {
           />
           <h3 className="text-lg">SM Anas</h3>
         </div>
-        <div className="mt-3">
+        <div className="mt-3 relative">
           <textarea
             className="border-gray-400 focus:outline-none rounded-md w-full "
             placeholder="What's on your mind?"
-            rows={5}
+            rows={imageShow ? 2 : 5}
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-        </div>
-        <div className="flex justify-between relative items-center">
-          <div className="w-12 cursor-pointer  rounded-lg h-12 bg-gradient-to-r from-violet-200 to-pink-200"></div>
-          <div
-            onClick={() => setEmoji((emoji) => !emoji)}
-            className="cursor-pointer "
-          >
-            <Feeling />
-          </div>
-          {emoji && (
-            <div className="absolute bottom-10 -right-40">
-              <EmojiPicker
-                searchDisabled={true}
-                skinTonesDisabled={true}
-                emojiStyle={EmojiStyle.FACEBOOK}
-                onEmojiClick={onEmojiClick}
-              />
+          {imageShow && (
+            <div className="">
+              <div
+                onClick={() => setEmoji((emoji) => !emoji)}
+                className="cursor-pointer absolute bottom-6 right-2"
+              >
+                <Feeling />
+              </div>
+              {emoji && (
+                <div className="absolute top-9 z-20 -right-40">
+                  <EmojiPicker
+                    searchDisabled={true}
+                    skinTonesDisabled={true}
+                    emojiStyle={EmojiStyle.FACEBOOK}
+                    onEmojiClick={onEmojiClick}
+                  />
+                </div>
+              )}
             </div>
           )}
         </div>
-        <div className="flex py-2 px-3 justify-between mt-3 border rounded-full">
-          <h3>Add To Your Post</h3>
-          <div className="flex cursor-pointer gap-2">
-            <Media />
-            <LiveIcon />
-            <CircleProfileIcon />
+        {!imageShow ? (
+          <>
+            <div className="flex justify-between relative items-center">
+              <div className="w-12 cursor-pointer  rounded-lg h-12 bg-gradient-to-r from-violet-200 to-pink-200"></div>
+              <div
+                onClick={() => setEmoji((emoji) => !emoji)}
+                className="cursor-pointer "
+              >
+                <Feeling />
+              </div>
+              {emoji && (
+                <div className="absolute bottom-10 -right-40">
+                  <EmojiPicker
+                    searchDisabled={true}
+                    skinTonesDisabled={true}
+                    emojiStyle={EmojiStyle.FACEBOOK}
+                    onEmojiClick={onEmojiClick}
+                  />
+                </div>
+              )}
+            </div>
+            <div className="flex py-2 px-3 justify-between mt-3 border rounded-full">
+              <h3>Add To Your Post</h3>
+              <div className="flex cursor-pointer gap-2">
+                <div onClick={() => setImageShow(true)}>
+                  <Media />
+                </div>
+                <LiveIcon />
+                <CircleProfileIcon />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="border">
+            <div className="py-16 relative m-1  flex justify-center items-center bg-gray-100">
+              <div
+                className="flex flex-col items-center"
+                onClick={() => choseRef.current?.click()}
+              >
+                <div
+                  className="
+                flex
+                justify-center items-center bg-primary-black text-white p-1 rounded-full"
+                >
+                  <Media />
+                </div>
+                <h3 className="py-1">Add Photo/Video</h3>
+                <span className="text-[12px]">or drag and drop</span>
+              </div>
+              <div
+                onClick={() => setImageShow(false)}
+                className="flex absolute top-1 cursor-pointer right-1 justify-center items-center text-gray-400"
+              >
+                <CircleCloseIcon />
+              </div>
+            </div>
           </div>
-        </div>
+        )}
         <button className="w-full border py-2 hover:text-white hover:bg-primary-black transition-all bg-[#f1f6ff] mt-4 rounded-md">
           Post
         </button>
       </div>
+      <input hidden type="file" accept="image/png, image/jpeg" ref={choseRef} />
     </div>
   );
 };
